b04aadbfa27530b8c4b45a79abded521e3909a45
Wutthichai Panyasung <wutibank@gmail.com>
2017-09-24 00:00:18 +0700

    Add AboutMe page

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    autoplay:true,
-    autoplayTimeout:1500,
-    autoplayHoverPause:false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        769:{
-            items:2,
-            nav:false
-        },
-        1008:{
-            items:4,
-            loop:true,
-        }
-    }
-});
-
 
         function openNav() {
             document.getElementById("myNav").style.height = "100%";
