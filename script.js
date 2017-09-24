194e349b82bbc6374ac50d10005c47adad4c7e33
Wutthichai Panyasung <wutibank@gmail.com>
2017-09-24 22:21:14 +0700

    Edit home back icon

--- script.js
+++ script.js
@@ -6,3 +6,21 @@
         function closeNav() {
             document.getElementById("myNav").style.height = "0%";
         }
+
+ //menu on scroll
+        $(window).scroll(function() {
+            if ($(this).scrollTop() >= 300) { // If page is scrolled more than 50px
+                $('#return-to-top').fadeIn(200); // Fade in the arrow
+                $('#menu-item').fadeIn(200); // Fade in the arrow
+                $('#menu-item-project').fadeIn(200); 
+            } else {
+                $('#return-to-top').fadeOut(200); // Else fade out the arrow
+                $('#menu-item').fadeOut(200); // Else fade out the arrow
+                $('#menu-item-project').fadeOut(200);
+            }
+        });
+        $('#return-to-top').click(function() { // When arrow is clicked
+            $('body,html').animate({
+                scrollTop: 0 // Scroll to top of body
+            }, 500);
+        });
